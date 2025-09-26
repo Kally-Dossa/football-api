8b57c0456209d99cec585850733415dd83607b96
Kally Dossa <85954491+Kally-Dossa@users.noreply.github.com>
2025-09-26 19:57:16 +0300

    railway url

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-const uri =
-  process.env.NODE_ENV === "production"
-    ? process.env.MONGO_URI
-    : process.env.MONGO_PUBLIC_URL;
+const mongoUri = process.env.MONGO_PUBLIC_URL || process.env.MONGO_URI;
 
 mongoose
-  .connect(uri)
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
