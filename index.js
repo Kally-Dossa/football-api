75d0468fadc50fdc218ff36fb0ea5259abc02d79
Kally Dossa <85954491+Kally-Dossa@users.noreply.github.com>
2025-09-26 19:50:49 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 
+const uri =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI
+    : process.env.MONGO_PUBLIC_URL;
+
 mongoose
-  .connect("mongodb://localhost:27017/FootballDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(uri)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const teamRoutes = require("./routes/teams");
 const playerRoutes = require("./routes/players");
