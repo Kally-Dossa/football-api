e8a8a29e0d5e7a9904d2c644597371806912831b
Kally Dossa <85954491+Kally-Dossa@users.noreply.github.com>
2025-09-27 12:08:51 +0300

    test get

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ const playerRoutes = require("./routes/players");
 app.use("/teams", teamRoutes);
 app.use("/players", playerRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello world.");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
